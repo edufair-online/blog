688932052b20f3482e00cfc5abbd8220da5215d1
Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-02-03 15:23:41 +0700

    init next-pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 // const isDev = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
-
+const withPlugins = require('next-compose-plugins')
+const withPWA = require('next-pwa')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-module.exports = withBundleAnalyzer({})
+const pwaLoader = withPWA({
+  target: 'serverless',
+
+  pwa: {
+    dest: 'public',
+    sourcemap: false
+  }
+})
+
+// Next configuration
+const nextConfig = {}
+
+module.exports = withPlugins([[withBundleAnalyzer], [pwaLoader], nextConfig])
